4b8f378b06f747cf57ea51c889734c7ffc83abfc
tamar.antebi <tamar.antebi@gmail.com>
2016-03-14 10:57:59 +0200

    simple rest server
    http://www.vogella.com/tutorials/REST/article.html#firstclient

---
+++ src/main/java/com/example/jersey/HelloWorld.java
@@ -0,0 +1,44 @@
+package com.example.jersey;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//base on this intro: http://www.vogella.com/tutorials/REST/article.html#firstclient
+// Plain old Java Object it does not extend as class or implements
+// an interface
+
+// The class registers its methods for the HTTP GET request using the @GET annotation.
+// Using the @Produces annotation, it defines that it can deliver several MIME types,
+// text, XML and HTML.
+
+// The browser requests per default the HTML MIME type.
+
+//Sets the path to base URL + /hello
+@Path("/hello")
+public class HelloWorld {
+
+    // This method is called if TEXT_PLAIN is request
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello() {
+        return "Hello dan";
+    }
+
+
+    // This method is called if XML is request
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String sayXMLHello() {
+        return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+    }
+
+    // This method is called if HTML is request
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHtmlHello() {
+        return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+                + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+    }
+
+}
